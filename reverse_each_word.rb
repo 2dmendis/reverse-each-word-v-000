42e236066d874b6668cc63d2eee1dab928521313
Dmitri Mendis <2dmendis@gmail.com>
2018-05-02 00:05:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_array = sentence.split(/ /)
   new_array.collect do |i|
-    i.reverse 
+    i.reverse! 
   end 
   final_array = new_array.join(" ")
   final_array
